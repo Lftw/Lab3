5d55ca856a4fb897ef78edc201d4d37787795e72
klokailo <klo.kai.lo@gmail.com>
2024-09-23 20:06:10 -0400

    Kai: Finished JSONTranslator

--- src/main/java/org/translation/JSONTranslator.java
+++ src/main/java/org/translation/JSONTranslator.java
@@ -5,9 +5,14 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * An implementation of the Translator interface which reads in the translation
@@ -15,7 +20,9 @@ import org.json.JSONArray;
  */
 public class JSONTranslator implements Translator {
 
-    // TODO Task: pick appropriate instance variables for this class
+    // This maps alpha3 code -> (Map from lang code -> country name)
+    private final Map<String, Map<String, String>> translatorMapper = new HashMap<>();
+    private final List<String> languageCodes;
 
     /**
      * Constructs a JSONTranslator using data from the sample.json resources file.
@@ -37,8 +44,24 @@ public class JSONTranslator implements Translator {
 
             JSONArray jsonArray = new JSONArray(jsonString);
 
-            // TODO Task: use the data in the jsonArray to populate your instance variables
-            //            Note: this will likely be one of the most substantial pieces of code you write in this lab.
+            // No duplicates!
+            Set<String> languageCodesSet = new HashSet<>();
+
+            for (Object baseJsonObj : jsonArray) {
+                JSONObject jsonObject = (JSONObject) baseJsonObj;
+                String alpha3 = jsonObject.getString("alpha3");
+                Map<String, String> langMapping = new HashMap<>();
+                for (String key : jsonObject.keySet()) {
+                    if ("alpha2".equals(key) || "alpha3".equals(key) || "id".equals(key)) {
+                        continue;
+                    }
+                    languageCodesSet.add(key);
+                    langMapping.put(key, jsonObject.getString(key));
+                }
+                this.translatorMapper.put(alpha3, langMapping);
+            }
+            // returns an immutable list
+            this.languageCodes = List.copyOf(languageCodesSet);
 
         }
         catch (IOException | URISyntaxException ex) {
@@ -48,21 +71,16 @@ public class JSONTranslator implements Translator {
 
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Task: return an appropriate list of language codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        return new ArrayList<>(this.translatorMapper.keySet());
     }
 
     @Override
     public List<String> getCountries() {
-        // TODO Task: return an appropriate list of country codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        return this.languageCodes;
     }
 
     @Override
     public String translate(String country, String language) {
-        // TODO Task: complete this method using your instance variables as needed
-        return null;
+        return this.translatorMapper.get(country).get(language);
     }
 }
